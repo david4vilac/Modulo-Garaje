c489118a40681e1df0438eacaa6551fe0cacab65
david4vilac <adavila59@ucatolica.edu.co>
2022-06-28 16:17:19 -0500

    Clase coches

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from typing_extensions import Required
 from odoo import models, fields, api
 
 class Aparcamiento(models.Model):
@@ -5,3 +7,25 @@ class Aparcamiento(models.Model):
     _description = 'Permite definir las caracteristicas de un aparcamiento.'
 
     name = fields.Char(string = 'Dirección', required = True)
+    plaza = fields.Integer(string = 'Plazas', requiered = True)
+
+
+class Coche(models.Model):
+    _name = 'garaje.coche'
+    _description = 'Permite definir las caracteristicas de un coche.'
+    _order = 'name'
+
+    name = fields.Char(string = 'Matricula', required = True, size = 7)
+    modelo = fields.Char(string = 'Modelo' )
+    construido = fields.Date(string = 'Fecha de construcción' )
+    consumo = fields.Float(string = 'Consumo', (4, 1), default = 0.0)
+    averiado = fields.Boolean(string = '')
+    annos = fields.Integer(string = 'Años', compute  = '_get_annos')
+    aparcamiento_id = fields.(string = '')
+    mantenimiento_ids = fields.(string = '')
+    description = fields.Text(string = 'Descripción')
+
+    @api.depends('construido'):
+    def _get_annos(self):
+        for coche in self:
+            coche.annos = 0
