dfa73de0af8fe69203f28c46b30aa738fc061994
david4vilac <adavila59@ucatolica.edu.co>
2022-06-30 10:29:00 -0500

    Seguridad - Grupos de Seguridad

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,8 @@
     'description' : "Este es un modulo de pruebas para la construccion de un modulo en Odoo 15.",
     'version':'0.1',
     'data': [
-        #'security/ir.model.access.csv'
+        'security/garaje_security.xml',
+        'security/ir.model.access.csv',
         'views/view.xml'
     ]
 }
